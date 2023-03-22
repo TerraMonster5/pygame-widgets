c42a164e6e77e826f7b444f11908592eb0394eca
TerraMonster5 <william.dargan@gmail.com>
2023-03-22 18:59:07 +0000

    Adjusted Button parameters and added .gitignore

--- src/widgets.py
+++ src/widgets.py
@@ -2,15 +2,16 @@ import pygame as pyg
 from typing import Union, Callable
 
 class Widget:
-    def __init__(self):
-        pass
+    def __init__(self, parent, x: int, y: int, text: str=""):
+        self._parent = parent
+        self._text = text
 
     def draw(self):
         pass
 
 class Button(Widget):
-    def __init__(self, parent, x: int, y: int, width: Union[int, None]=None, height: Union[int, None]=None, state: str="enabled", default: str="enabled", text: str="", command: Union[Callable, None]=None) -> None:
-        pass
+    def __init__(self, parent, x: int, y: int, width: Union[int, None]=None, height: Union[int, None]=None, bg: Union[pyg.Color, str, None]=None, state: str="enabled", default: str="enabled", text: str="", command: Union[Callable, None]=None) -> None:
+        super().__init__(parent, x, y, text)
 
 class Entry(Widget):
     pass
