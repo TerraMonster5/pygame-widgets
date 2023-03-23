74f8e447b6bb14cf3604f017fecd41475bb52428
TerraMonster5 <william.dargan@gmail.com>
2023-03-23 22:05:52 +0000

    Added code for drawing the Label

--- src/widgets.py
+++ src/widgets.py
@@ -1,34 +1,66 @@
-import pygame as pyg
+import pygame
+import pygame.freetype
 from typing import Union, Callable
 
-class Widget:
-    def __init__(self, parent, x: int, y: int, text: str=""):
-        self._parent = parent
-        self._text = text
+font = pygame.freetype.SysFont("Arial", 18)
 
-    def draw(self):
-        pass
-
-class Label(Widget):
-    pass
-
-class Button(Widget):
+class Label:
     def __init__(self,
                  parent,
-                 x: int,
-                 y: int,
+                 x: int=0,
+                 y: int=0,
+                 text: str="") -> None:
+        
+        """
+        Displays a text box with top left co-ordinates at x, y containing text.
+        """
+
+        self.parent = parent
+        self.x = x
+        self.y = y
+        self.text = text
+
+    def draw(self) -> None:
+        font.render_to(self.parent, (self.x, self.y), self.text)
+
+class Button:
+    def __init__(self,
+                 parent,
+                 x: int=0,
+                 y: int=0,
                  width: Union[int, None]=None,
                  height: Union[int, None]=None,
-                 bg: Union[pyg.Color, str, None]=None,
+                 bg: Union[pygame.Color, int, tuple, None]=None,
                  state: str="enabled",
-                 default: str="enabled",
                  text: str="",
                  command: Union[Callable, None]=None) -> None:
         
         """
+        Displays and interactive button with top left co-ordinates at x, y containing text.
         """
 
-        super().__init__(parent, x, y, text)
+        self.parent = parent
+        self.x = x
+        self.y = y
+
+        if width: self.width = width
+        else: self.width = 100
+
+        if height: self.height = height
+        else: self.height = 50
+
+        if bg: self.bg = bg
+        else: self.bg = pygame.Color(0, 0, 0)
+
+        self.state = state
+        self.text = text
+        self.command = command
+    
+    def draw(self) -> None:
+        pygame.draw.rect(self.parent, self.bg, pygame.Rect(self.x, self.y, self.width, self.height), 0)
+
+        if self.text:
+            text = font.render(self.text, 1, (0, 0, 0))
 
 class CheckButton(Button):
     pass
@@ -37,5 +69,5 @@ class RadioButton(Button):
     __groups: dict = {}
     pass
 
-class Entry(Widget):
+class Entry:
     pass
